7f1dc7b497a5fe0c1821237c9cbe12de6dc05185
tsukada-cs <tsukada.cs@gmail.com>
2023-08-14 18:19:38 +0900

    add cron01.sh

---
+++ scripts/cron01.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Functions
+run_python_script() {
+    $HOME/anaconda3/envs/tc/bin/python "$@"
+}
+
+# Activate conda environment
+conda activate tc
+
+# Sync
+bash $HOME/git/realtimeTC/scripts/sync_remote.sh
+
+# B01 acquire bset track
+typeset -a basins=("AL" "EP" "WP" "IO" "SH")
+year=$(date +%Y)
+for basin in "${basins[@]}"; do
+    echo "=====" $year $basin "====="
+    run_python_script $HOME/git/realtimeTC/scripts/b01_update_btk.py -y $year -b $basin -p $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/JTWC_pre_btk
+done
+
+current_month=$(date +%m)
+if [ $current_month -eq 01 ]; then
+    lastyear=$((current_year - 1))
+    for basin in "${basins[@]}"; do
+        run_python_script $HOME/git/realtimeTC/scripts/b01_update_btk.py -y $lastyear -b $basin -p $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/JTWC_pre_btk
+    done
+fi
+
+# B02 listup latest IDs
+run_python_script $HOME/git/realtimeTC/scripts/b02_listup_updated_IDs.py $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/latest_IDlist.csv
+
+# B03 acquire SAR ATCF
+run_python_script $HOME/git/realtimeTC/scripts/b03_acquire_NESDIS_SAR_ATCF.py
+
+# C01(P01) plot best track intensity
+run_python_script $HOME/git/realtimeTC/scripts/c01_call_p01.py --plot_NESDIS_SAR
+
+# H01 generate HTML
+run_python_script $HOME/git/realtimeTC/scripts/h01_generate_html.py
+
+# Rsync images
+rsync -av $HOME/git/realtimeTC/outputs/JTWC_pre_intensity/ tc-times@www1163.sakura.ne.jp:www/tmp/
+
+# Rsync tclist.csv
+rsync -av $HOME/git/realtimeTC/refdata/TCs/tclist.csv tc-times@www1163.sakura.ne.jp:tsukada/share/
