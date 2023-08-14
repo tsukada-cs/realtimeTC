f39b54d3c598aebe135071aaedcad505d6edaea2
tsukada-cs <tsukada.cs@gmail.com>
2023-08-14 18:41:10 +0900

    add name acq

--- scripts/Realtime.py
+++ scripts/Realtime.py
@@ -208,8 +208,8 @@ def _read_jtwc_bt_from_rows(rows):
     ds["pres"] = ds["pres"].where(ds["pres"]>0)
     ds["vmax_kt"] = ds["vmax_kt"].where(ds["vmax_kt"]>0)
     ds["vmax"] = knot_to_ms(ds["vmax_kt"])
-    ds["nnb"] = df["nnb"][0]
-    ds["name"] = df["name"][-1]
+    ds["nnb"] = df["nnb"].iloc[0]
+    ds["name"] = df["name"].iloc[-1]
     ds["year"] = ds["time"][0].dt.year.item()
     basin = {"L":"AL", "E":"EP", "W":"WP", "A":"IO", "B":"IO", "S":"SH", "P":"SH"}[ds.nnb.item()[-1]]
     ds["bbnnyyyy"] = basin+ds.nnb.item()[:2]+str(ds.year.item())
