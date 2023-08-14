30ead856df438239c8c4c11a26758effbf550191
tsukada-cs <tsukada.cs@gmail.com>
2023-08-15 00:02:14 +0900

    update cron

--- scripts/cron01.sh
+++ scripts/cron01.sh
@@ -14,25 +14,26 @@ bash $HOME/git/realtimeTC/scripts/sync_remote.sh
 # b01 acquire bset track
 echo "---------------- b01 ----------------"
 basins="AL EP WP IO SH"
+# basins="WP"
 year=$(date +%Y)
 run_python_script $HOME/git/realtimeTC/scripts/b01_update_btk.py -y $year -b $basins -p $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/JTWC_pre_btk
 
-current_month=$(date +%m)
-if [ $current_month -eq 01 ]; then
-lastyear=$((current_year - 1))
-run_python_script $HOME/git/realtimeTC/scripts/b01_update_btk.py -y $lastyear -b $basins -p $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/JTWC_pre_btk
-fi
+# current_month=$(date +%m)
+# if [ $current_month -eq 01 ]; then
+# lastyear=$((current_year - 1))
+# run_python_script $HOME/git/realtimeTC/scripts/b01_update_btk.py -y $lastyear -b $basins -p $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/JTWC_pre_btk
+# fi
 
 # b02 listup latest IDs
 echo "---------------- b02 ----------------"
 time_cutoff=48
-run_python_script $HOME/git/realtimeTC/scripts/b02_listup_updated_IDs.py $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/latest_IDlist.csv --time_cutoff $time_cutoff
+run_python_script $HOME/git/realtimeTC/scripts/b02_listup_updated_IDs.py $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/latest_IDlist.csv -y $year -b $basins --time_cutoff $time_cutoff
 
 # b03 acquire SAR ATCF
 echo "---------------- b03 ----------------"
 run_python_script $HOME/git/realtimeTC/scripts/b03_acquire_NESDIS_SAR_ATCF.py
 
-# b04 acquire SAR ATCF
+# b04 acquire JMA btk
 echo "---------------- b04 ----------------"
 run_python_script $HOME/git/realtimeTC/scripts/b04_acquire_pre_JMA_btk.py
 
