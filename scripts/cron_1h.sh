8002c11739fcc8c6134aadf3748b6d2e5a4597c6
tsukada-cs <tsukada.cs@gmail.com>
2023-08-14 14:45:49 +0900

    fix cron

--- scripts/cron_1h.sh
+++ scripts/cron_1h.sh
@@ -5,7 +5,6 @@ year=$(date +%Y)
 for basin in "${basins[@]}"; do
     echo "=====" $year $basin "====="
     $HOME/anaconda3/envs/tc/bin/python $HOME/git/realtimeTC/scripts/b01_update_btk.py -y $year -b $basin -p $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/JTWC_pre_btk
-    $HOME/anaconda3/envs/tc/bin/python $HOME/git/realtimeTC/scripts/b02_listup_updated_IDs.py -o $HOME/git/realtimeTC/refdata/TCs/latest_IDlist.csv
 done
 
 current_month=$(date +%m)
@@ -13,9 +12,10 @@ if [ $current_month -eq 01 ]; then
     lastyear=$((current_year - 1))
     for basin in "${basins[@]}"; do
         $HOME/anaconda3/envs/tc/bin/python $HOME/git/realtimeTC/scripts/b01_update_btk.py -y $lastyear -b $basin -p $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/JTWC_pre_btk
-        $HOME/anaconda3/envs/tc/bin/python $HOME/git/realtimeTC/scripts/b02_listup_updated_IDs.py -o $HOME/git/realtimeTC/refdata/TCs/latest_IDlist.csv
     done
 fi
+
+$HOME/anaconda3/envs/tc/bin/python $HOME/git/realtimeTC/scripts/b02_listup_updated_IDs.py $HOME/git/realtimeTC/refdata/TCs/tclist.csv -o $HOME/git/realtimeTC/refdata/TCs/latest_IDlist.csv
 
 rsync -av $HOME/git/realtimeTC/outputs/JTWC_pre_intensity/ tc-times@www1163.sakura.ne.jp:www/tmp/
 rsync -av $HOME/git/realtimeTC/refdata/TCs/tclist.csv tc-times@www1163.sakura.ne.jp:tsukada/share/
