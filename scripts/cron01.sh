05ec02f73520a925074513c10220a235a8369b18
tsukada-cs <tsukada.cs@gmail.com>
2023-08-16 22:12:49 +0900

    bashnize cron01

--- scripts/cron01.sh
+++ scripts/cron01.sh
@@ -19,7 +19,7 @@ echo "---------------- b01 ----------------"
 basins="AL EP WP IO SH"
 # basins="WP"
 year=$(date +%Y)
-run_python_script scripts/b01_update_btk.py -y $year -b $=basins -p refdata/TCs/tclist.csv -o refdata/TCs/JTWC_pre_btk
+run_python_script scripts/b01_update_btk.py -y $year -b $basins -p refdata/TCs/tclist.csv -o refdata/TCs/JTWC_pre_btk
 
 # current_month=$(date +%m)
 # if [ $current_month -eq 01 ]; then
@@ -30,7 +30,7 @@ run_python_script scripts/b01_update_btk.py -y $year -b $=basins -p refdata/TCs/
 # b02 listup latest IDs
 echo "---------------- b02 ----------------"
 time_cutoff=2
-run_python_script scripts/b02_listup_updated_IDs.py refdata/TCs/tclist.csv -o refdata/TCs/latest_IDlist.csv -y $year -b $=basins --time_cutoff $time_cutoff
+run_python_script scripts/b02_listup_updated_IDs.py refdata/TCs/tclist.csv -o refdata/TCs/latest_IDlist.csv -y $year -b $basins --time_cutoff $time_cutoff
 
 # b03 acquire SAR ATCF
 echo "---------------- b03 ----------------"
