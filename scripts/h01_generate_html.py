91922427dd40f58b9f3c8ab67fd17cc94a88709a
tsukada-cs <tsukada.cs@gmail.com>
2023-08-17 23:36:56 +0900

    fix h01

--- scripts/h01_generate_html.py
+++ scripts/h01_generate_html.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 
 
-def generate_filelist_html(image_paths, names):
+def generate_filelist_html(image_paths, names, show_years):
     html = '''
     <!DOCTYPE html>
     <html lang="en">
@@ -59,7 +59,7 @@ def generate_filelist_html(image_paths, names):
     '''
 
     basins = ["AL","EP","WP","IO","SH"]
-    for year in years:
+    for year in show_years:
         basinwise_URLs = {"AL":[], "EP":[], "WP":[], "IO":[], "SH":[]}
         basinwise_names = {"AL":[], "EP":[], "WP":[], "IO":[], "SH":[]}
         for image_path, name in zip(image_paths, names):
@@ -79,7 +79,7 @@ def generate_filelist_html(image_paths, names):
             html += f'<p class="h6 text-body">{basin}</p>\n'
             for image_path, name in zip(basinwise_URLs[basin], basinwise_names[basin]):
                 bbnnyyyy = os.path.basename(image_path)[:8]
-                html += f'    <a href="{image_path}" class="text-info fs-6 image-link">{bbnnyyyy} / {name}</a>\n<br>\n'
+                html += f'    <a href="{bbnnyyyy[-4:]}/{bbnnyyyy}/index.html" class="text-info fs-6 image-link">{bbnnyyyy} / {name}</a>\n<br>\n'
             html += '</div>\n'
         html += '</div>\n'
         html += '</>\n'
@@ -87,10 +87,9 @@ def generate_filelist_html(image_paths, names):
     html += '''
     <footer class="footer">
     <div class="text-center mt-4 py-4 bg-light border-top border-bottom">
-        <a href="index.html" class="text-muted align-middle">&copy; Vortex Vision</a>
+        <a href="../index.html" class="text-muted align-middle">&copy; Vortex Vision</a>
     </div>
     </footer>
-
     </body>
     </html>
     '''
@@ -133,7 +132,6 @@ def generate_image_viewer_html(image_paths, names):
     '''
     for path, name in zip(image_paths[1:], names[1:]):
         html += f'<option value="{path}">{os.path.basename(path)[:8]} / {name}</option>'
-
     html += f'''
                     </select>
                 </div>
@@ -158,38 +156,82 @@ def generate_image_viewer_html(image_paths, names):
         </script>
     <footer class="footer">
     <div class="text-center mt-4 py-4 bg-white">
-        <a href="index.html" class="text-muted align-middle">&copy; Vortex Vision</a>
+        <a href="../index.html" class="text-muted align-middle">&copy; Vortex Vision</a>
     </div>
     </footer>
     </body>
     </html>
     '''
-
     return html
 
 
+def generate_TC_html(image_path, name):
+    bbnnyyyy = os.path.basename(image_path)[:8]
+    html = f'''
+    <html>
+    <head>
+        <title>{bbnnyyyy} / {name} </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    '''
+    html += '''
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YL02WM0DDQ"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-YL02WM0DDQ');
+    </script>
+    </head>
+    '''
+
+    html += f'''
+    <body class="bg-white">
+        <div class="container-fluid px-3">
+            <div class="row pt-3">
+                <div class="col text-center my-3">
+                    <p class="lead mb-2" style="color: #0a2f35;">{bbnnyyyy} / {name}</p>
+                    <span class="badge bg-danger text-light px-2 py-1">beta</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-7 col-12 mx-auto" id="displayed-image-container">
+                    <img id="displayed-image" class="img-fluid" src="{image_path}" alt="Intensity">
+                </div>
+            </div>
+        </div>
+    '''
+    html += '''
+    <footer class="footer">
+    <div class="text-center mt-4 py-4 bg-white">
+        <a href="../../../index.html" class="text-muted align-middle">&copy; Vortex Vision</a>
+    </div>
+    </footer>
+    </body>
+    </html>
+    '''
+    return html
 #%%
-# btk_archive.html
 tclist = pd.read_csv(f"{os.environ['HOME']}/git/realtimeTC/data/tclist.csv", skipinitialspace=True)
 tclist = tclist.sort_values("ID")
 image_paths = list("../data/TCs/" + tclist["ID"].str[-4:] + "/" + tclist["ID"] + "/outputs/" + tclist["ID"]+"_intensity.png")
 names = tclist["name"].values.tolist()
+show_years = np.arange(2023,2021-1,-1).astype(str)
 
-years = np.arange(2023,2021-1,-1).astype(str)
-html_content = generate_filelist_html(image_paths, names)
-
+# [btk_archive.html]
+html_content = generate_filelist_html(image_paths, names, show_years)
 with open(f"{os.environ['HOME']}/git/realtimeTC/outputs/html/btk_archive.html", "w") as file:
     file.write(html_content)
 
 
-# image_viewer.html
+# [image_viewer.html]
 path_texts = " ".join(image_paths)
 pattern = r'[A-Z][A-Z][0-4][0-9](\d{4})_intensity.png'
 path_years = np.array(re.findall(pattern, path_texts))
 
 image_paths_sorted = []
 names_sorted = []
-for year in years:
+for year in show_years:
     image_paths_sorted = np.r_[image_paths_sorted, np.array(image_paths)[path_years==year]]
     names_sorted = np.r_[names_sorted, np.array(names)[path_years==year]]
 
@@ -197,4 +239,15 @@ html_content = generate_image_viewer_html(image_paths_sorted, names_sorted)
 with open(f"{os.environ['HOME']}/git/realtimeTC/outputs/html/image_viewer.html", "w") as file:
     file.write(html_content)
 
+
+# [ex: WP072023.html]
+for image_path, name in zip(image_paths, names):
+    html_content = generate_TC_html("../../"+image_path, name)
+
+    bbnnyyyy = os.path.basename(image_path)[:8]
+    year = bbnnyyyy[-4:]
+    tchtml_dir = f"{os.environ['HOME']}/git/realtimeTC/outputs/html/{year}/{bbnnyyyy}"
+    os.makedirs(tchtml_dir, exist_ok=True)
+    with open(f"{tchtml_dir}/index.html", "w") as file:
+        file.write(html_content)
 # %%
